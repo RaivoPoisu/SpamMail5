e1923f992f8f9b17959a433dd95bcf029b9f7b6f
KaarenTenson <tensonkaaren@gmail.com>
2024-11-26 22:09:21 +0200

    sain uue mudeliga testitud, tegin uue funktsioonide mudelite salvestamiseks ning failist lugemiseks

--- Model.py
+++ Model.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import pickle
 
 def RandomForestModel(SpamData):
     
@@ -9,7 +10,7 @@ def RandomForestModel(SpamData):
     
     vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)  # You can tune max_features
     X = vectorizer.fit_transform(SpamData['text'])
-
+   
 # Step 3: Define target variable
     y = SpamData['label']
     
@@ -27,3 +28,19 @@ def RandomForestModel(SpamData):
     accuracy = accuracy_score(y_test, y_pred)
     print(f'Accuracy: {accuracy * 100:.2f}%')
     return model
+def TestModel(SpamDataTest, model):
+    SpamDataTest['text'] = SpamDataTest['text'].apply(lambda x: ' '.join(word.replace('\n', ' ') for word in x.split())) #Removing the \n and treating the words as 2 words
+    vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)  # You can tune max_features
+    X_test = vectorizer.fit_transform(SpamDataTest['text'])
+    y_test= SpamDataTest["label"]
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+def SaveModel(model, name):
+    model_pkl_file = name+".pkl"  
+    with open(model_pkl_file, 'wb') as file:  
+        pickle.dump(model, file)
+def ReadModel(fileName):
+    fileName=fileName+ ".pkl"
+    with open(fileName, 'rb') as file:  
+        model = pickle.load(file)
